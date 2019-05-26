c7e25d53d9faea4f416c243ec768cb12bc2e8650
Paolo <dino.tartaro@gmail.com>
2019-05-26 08:33:57 +0200

    Update rtl8192cu-fix-script.sh

--- rtl8192cu-fix-script.sh
+++ rtl8192cu-fix-script.sh
@@ -61,7 +61,7 @@ dri=`sudo dkms status 8192cu/1.11 | grep -o 'installed'`
 if [[ "$dri" == "installed" ]]; then
 	read -p "This driver is already installed, do you want to remove it now? [y/n]: " un
 	if [[ "$un" == "y" ]]; then
-		sudo -n dkms uninstall 8192cu/1.11 --all
+		sudo -n dkms uninstall 8192cu/1.11
 		rm -f /etc/modprobe.d/blacklist-native-rtl8192.conf 2>/dev/null
 		rm -f /etc/modprobe.d/8192cu-disable-power-management.conf 2>/dev/null
 		echo "The driver was removed"
